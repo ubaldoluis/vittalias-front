958dd33916150b3471e9f5aa5f0d12e903606d33
Moravecs <ubaldo.luis@tpmcorp.es>
2025-04-16 13:44:41 +0200

    chore: Remove .open-next from git and add to .gitignore

--- .open-next/cloudflare-templates/worker.d.ts
+++
@@ -1,4 +0,0 @@
-export { DOQueueHandler } from "./.build/durable-objects/queue.js";
-export { DOShardedTagCache } from "./.build/durable-objects/sharded-tag-cache.js";
-declare const _default: ExportedHandler<CloudflareEnv>;
-export default _default;
